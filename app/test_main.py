55244bab0c646fc7181265155d85a9f7b4ff8143
GhostKlinkz <taralaroslav@gmail.com>
2026-08-26 21:30:44 +0300

    Add type hints to test functions in test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,24 +1,25 @@
+from typing import Any
 from unittest.mock import patch
 
 from app.main import cryptocurrency_action
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_buy_more_cryptocurrency(mock_prediction) -> None:
+def test_buy_more_cryptocurrency(mock_prediction: Any) -> None:
     mock_prediction.return_value = 106
 
     assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_sell_all_cryptocurrency(mock_prediction) -> None:
+def test_sell_all_cryptocurrency(mock_prediction: Any) -> None:
     mock_prediction.return_value = 94
 
     assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing(mock_prediction) -> None:
+def test_do_nothing(mock_prediction: Any) -> None:
     mock_prediction.return_value = 100
 
     assert cryptocurrency_action(100) == "Do nothing"
@@ -26,7 +27,7 @@ def test_do_nothing(mock_prediction) -> None:
 
 @patch("app.main.get_exchange_rate_prediction")
 def test_do_nothing_when_prediction_is_exactly_5_percent_higher(
-    mock_prediction,
+    mock_prediction: Any,
 ) -> None:
     mock_prediction.return_value = 105
 
@@ -35,7 +36,7 @@ def test_do_nothing_when_prediction_is_exactly_5_percent_higher(
 
 @patch("app.main.get_exchange_rate_prediction")
 def test_do_nothing_when_prediction_is_exactly_5_percent_lower(
-    mock_prediction,
+    mock_prediction: Any,
 ) -> None:
     mock_prediction.return_value = 95
 
